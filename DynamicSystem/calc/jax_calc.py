86fcef74efaf73ae34436e85cda876a29e061f07
hamzahjausi <2021901023@sys.yu.edu.jo>
2024-05-25 00:10:03 +0300

    used einsum to improve the calc speed !

--- DynamicSystem/calc/jax_calc.py
+++ DynamicSystem/calc/jax_calc.py
@@ -1,40 +1,34 @@
-from jax import lax ,jit,vmap
+from jax import lax, jit, vmap
 import jax.numpy as jnp
 import jax
-jax.config.update ("jax_enable_x64", True)
+
+jax.config.update("jax_enable_x64", True)
 
 
-def fast_calc (n,
-               include_init = False 
-               ):
+def fast_calc(n, include_init=False):
 
-    def rollout_fn (U_matrix , Un ):
-        def scan_body (Un ,_) :
-            Un_1 = U_matrix@ Un
-            return Un_1 , Un_1
-        _ , history = lax.scan (jit(scan_body , backend="cpu") ,Un , None ,
-                                 length= n)
-        if include_init :
-            return jnp.concatenate ([
-                jnp.expand_dims(Un , axis =0),history] ,
-                axis = 0 
-            )
+    def rollout_fn(U_matrix, Un):
+        def scan_body(Un, _):
+            Un_1 = jnp.einsum("ij , j ", U_matrix, Un)
+            return Un_1, Un_1
+
+        _, history = lax.scan(jit(scan_body, backend="cpu"), Un, None, length=n)
+        if include_init:
+            return jnp.concatenate([jnp.expand_dims(Un, axis=0), history], axis=0)
         return history
-    
+
     return rollout_fn
 
 
-def fast_analytical_solver (X,
-                            Lambda,
-                            c):
-    vectorize = lambda func : vmap (jit(func))
+def fast_analytical_solver(X, Lambda, c):
+    vectorize = lambda func: vmap(jit(func))
 
-    def process (t):
-        
-        def calc (t):
-            Lambda_t_exp = jnp.diag(jnp.exp(Lambda*t))
-            return X@Lambda_t_exp@c
+    def process(t):
+
+        def calc(t):
+            Lambda_t_exp = jnp.diag(jnp.exp(Lambda * t))
+            return jnp.einsum("ij,jh,h", X, Lambda_exp, c)
 
         return vectorize(calc)(t)
 
-    return process 
+    return process
--- DynamicSystem/main.py
+++ DynamicSystem/main.py
@@ -1,6 +1,6 @@
-import jax as jnp
 from calc.jax_calc import *
 from methods.numarical_methods import *
+import jax.numpy as jnp
 import jax
 
 jax.config.update("jax_enable_x64", True)
@@ -13,12 +13,11 @@ A = jnp.array([[-0.1, 1], [-1, -0.2]], dtype=jnp.float64)
 dt = 0.1
 ini = jnp.array([1, 0], dtype=jnp.float64)
 Uf = forward_setup(A, dt)
-t = jnp.linspace(0, 2 * jnp.pi, 63, dtype=jnp.float64)
-U1 = fast_calc(64)(Uf, ini)
+U1 = fast_calc(62 ,include_init=True)(Uf, ini)
 u1, u2 = U1.T
 
 Ub = backward_setup(A, dt)
-U2 = fast_calc(64)(Ub, ini)
+U2 = fast_calc(63,include_init=True)(Ub, ini)
 a1, a2 = U2.T
 
 
