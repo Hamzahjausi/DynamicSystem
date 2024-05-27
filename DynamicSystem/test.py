22163093ec51ecd33f2782512d273693793b9595
hamzahjausi <2021901023@sys.yu.edu.jo>
2024-05-27 19:53:21 +0300

    new methood

--- DynamicSystem/test.py
+++ DynamicSystem/test.py
@@ -1,7 +1,9 @@
+import jax
 import jax.numpy as jnp
-from jax import lax, config, jit, vmap
-from calc.jax_calc import *
+from jax import lax, jit, config
 import matplotlib.pyplot as plt
+
+config.update("jax_enable_x64", True)
 
 
 def forward_setup(system_matrix, dt):
@@ -9,33 +11,52 @@ def forward_setup(system_matrix, dt):
     return U_matrix
 
 
-A = jnp.array([[0, 1], [-1, 0]])
+def fast_calc(n, include_init=False):
+
+    def rollout_fn(U_matrix, Un):
+        def scan_body(Un, _):
+            Un_1 = U_matrix @ Un
+            return Un_1, Un_1
+
+        _, history = lax.scan(jit(scan_body, backend="cpu"), Un, None, length=n)
+        if include_init:
+            return jnp.concatenate([jnp.expand_dims(Un, axis=0), history], axis=0)
+        return history
+
+    return rollout_fn
 
 
 def central_setup(system_matrix, dt):
     U_matrix = 2 * dt * system_matrix
-    I = jnp.identity(system_matrix.shape[0])
-    return U_matrix, I
+    I_matrix = jnp.eye(system_matrix.shape[0])
+    return U_matrix, I_matrix
 
 
-u0 = jnp.array([1, 0])
+def Fast_calc(n):
+    def rollout_fn(UC, UN):
+        def scan_body(INI, _):
+            step = jnp.einsum("ij,j->i", UC[1], INI[0]) + jnp.einsum(
+                "ij,j->i", UC[0], INI[1]
+            )
+            INI = jnp.roll(INI, shift=-1, axis=0).at[-1].set(step)
+            return INI, step
+
+        _, history = lax.scan(scan_body, UN, None, length=n)
+        return history
+
+    return rollout_fn
+
+
+A = jnp.array([[0, 1], [-1, -0.1]], dtype=jnp.float64)
 dt = 0.01
+ini = jnp.array([1, 0], dtype=jnp.float64)
+n = int(2 * jnp.pi / dt)
 UC = central_setup(A, dt)
 UF = forward_setup(A, dt)
-u1 = UF @ u0
-n_steps = 630
-U = jnp.zeros(shape=(n_steps, 2))
+ini_next_step = fast_calc(1)(UF, ini)
 
-U = U.at[0].set(u0)
-U = U.at[1].set(u1)
-
-for i in range(1, n_steps):
-    U = U.at[i + 1].set(UC[0] @ U[i] + UC[1] @ U[i - 1])
-
-u1, u2 = U.T
-
-plt.plot(u1, u2, "-ro")
-plt.grid(True)
-plt.ylim(-2, 2)
-plt.xlim(-2, 2)
+INI = jnp.stack([ini, *ini_next_step])
+calc_fn = Fast_calc(n)
+U = calc_fn(UC, INI).T
+plt.plot(*U)
 plt.show()
